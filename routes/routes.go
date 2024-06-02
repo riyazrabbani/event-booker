79d01e90496e1911ed9af3422f62f6f74dda66d7
Riyaz Rabbani <146787234+riyazrabbani@users.noreply.github.com>
2024-06-02 16:13:18 +0000

    added event deletion

--- routes/routes.go
+++ routes/routes.go
@@ -6,4 +6,6 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
+	server.DELETE("/events/:id", deleteEvent)
 }
