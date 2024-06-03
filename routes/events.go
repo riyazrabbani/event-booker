fec364bdac084e02b9efc12095d57f5dff702106
Riyaz Rabbani <riyaz.rabbani@hotmail.com>
2024-06-03 13:12:19 +0000

    added eventIDs & route protection

--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"example.com/EventBooker/models"
+	"example.com/EventBooker/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -42,16 +43,22 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
-	event.ID = 1
-	event.UserID = 1
+	event.UserID = userId
 
 	err = event.Save()
 
